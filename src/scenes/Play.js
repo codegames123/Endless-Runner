34f97aab5df64532f7dac7a2a63f7758fbfba0b2
codegames123 <103101101+codegames123@users.noreply.github.com>
2022-04-24 01:04:41 -0700

    more changes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,4 +1,5 @@
 var addScore;
+var platforms
 class Play extends Phaser.Scene{
     constructor() {
         super("playScene");
@@ -6,9 +7,12 @@ class Play extends Phaser.Scene{
 
     preload(){
         this.load.path = 'assets/';
-        this.load.atlas('sprite', 'rocket1.png', 'kenny_sheet.json'); //atlas temporary example test. will change everything later
+        //this.load.atlas('sprite', 'rocket1.png', 'kenny_sheet.json'); //atlas temporary example test. will change everything later
+        this.load.spritesheet('player', 'spaceshipanimation.png',{ frameWidth: 113, frameHeight: 48, startFrame: 0, endFrame: 7 });
+        this.load.image('rock', 'rock.png');
         this.load.image('background', 'background.jpg');
         this.load.image('ground','ground.png');
+        
     }
     create() {
         // variables and settings
@@ -17,12 +21,17 @@ class Play extends Phaser.Scene{
         this.SCROLL_SPEED = 4;
         currentScene = 3;
         this.physics.world.gravity.y = 2600;
+        
+        this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0);
 
+        platforms = this.physics.add.image(game.config.width/2 + 420, game.config.height/2 + 228, 'ground').setScale(2).setSize(40, 20).setOffset(30, 8);
+        //platforms.create(game.config.width/2, game.config.height/2 + 228, 'ground').setScale(2) 
+        platforms.setImmovable(true);
+        platforms.body.allowGravity = false;
         // play background music
         let song = this.sound.add('baby_song', {loop: true}); 
-        song.play();
-        // add tile sprite
-        this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0);
+        //song.play(); //plays song
+        
         let textConfig = {
             fontFamily: 'Courier',
             fontSize: '29px',
@@ -40,24 +49,25 @@ class Play extends Phaser.Scene{
         this.pointText = this.add.text(game.config.width/2 + 200,game.config.height/2 - 270, 'Points: ', textConfig);
         this.scoreLeft = this.add.text(game.config.width/2 + 350,game.config.height/2 - 270,  this.pScore, textConfig);
         // make ground tiles group
-        this.ground = this.add.group();
-        for(let i = 0; i < game.config.width; i += tileSize) {
-            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'sprite', 'block').setScale(SCALE).setOrigin(0);
-            groundTile.body.immovable = true;
-            groundTile.body.allowGravity = false;
-            this.ground.add(groundTile);
-        }
+        //platforms = this.add.group();
+        // for(let i = 0; i < game.config.width; i += tileSize) {
+        //     let groundTile = this.physics.add.sprite(i, game.config.height - 50, 'ground').setScale(SCALE).setOrigin(0);
+        //     groundTile.body.immovable = true;
+        //     groundTile.body.allowGravity = false;
+        //     platforms.add(groundTile);
+        // }
         // put another tile sprite above the ground tiles
-        this.groundScroll = this.add.tileSprite(0, game.config.height-tileSize, game.config.width, tileSize, 'ground').setOrigin(0);
-
-        this.player = this.physics.add.sprite(game.config.width/6, game.config.height/2-tileSize, 'sprite', 'side').setScale(SCALE);
+       // this.groundScroll = this.add.tileSprite(0, game.config.height-tileSize, game.config.width, tileSize, 'ground').setSize(24, 20).setOffset(8, 12).setOrigin(0);
+        
+        this.player = this.physics.add.sprite(game.config.width/9, game.config.height/2+150, 'player').setScale(SCALE);
+        this.player.setCollideWorldBounds(true);
 
         // set up Phaser-provided cursor key input
         cursors = this.input.keyboard.createCursorKeys();
         
         // add physics collider
-        this.physics.add.collider(this.player, this.ground);
-
+        this.physics.add.collider(this.player, platforms);
+        
         addScore = this.time.addEvent({ delay: 1000, callback: this.addToScore, callbackScope: this, loop: true }); //calls addToScore every second
     }
 
@@ -69,7 +79,8 @@ class Play extends Phaser.Scene{
     update() {
         // update tile sprites (tweak for more "speed")
         this.background.tilePositionX += this.SCROLL_SPEED;
-        this.groundScroll.tilePositionX += this.SCROLL_SPEED;
+        //this.groundScroll.tilePositionX += this.SCROLL_SPEED;
+        //this.groundScroll.tilePositionY += this.SCROLL_SPEED;
 
 		// check if alien is grounded
 	    this.player.isGrounded = this.player.body.touching.down;
