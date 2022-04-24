e554ddc8ad5a09ac74e56c3a1702b18bf0b0f386
codegames123 <103101101+codegames123@users.noreply.github.com>
2022-04-23 18:12:29 -0700

    commit changes

---
+++ src/scenes/Play.js
@@ -0,0 +1,95 @@
+var addScore;
+class Play extends Phaser.Scene{
+    constructor() {
+        super("playScene");
+    }
+
+    preload(){
+        this.load.path = 'assets/';
+        this.load.atlas('sprite', 'rocket1.png', 'kenny_sheet.json'); //atlas temporary example test. will change everything later
+        this.load.image('background', 'background.jpg');
+        this.load.image('ground','ground.png');
+    }
+    create() {
+        // variables and settings
+        this.JUMP_VELOCITY = -700;
+        this.MAX_JUMPS = 2;
+        this.SCROLL_SPEED = 4;
+        currentScene = 3;
+        this.physics.world.gravity.y = 2600;
+
+        // play background music
+        let song = this.sound.add('baby_song', {loop: true}); 
+        song.play();
+        // add tile sprite
+        this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0);
+        let textConfig = {
+            fontFamily: 'Courier',
+            fontSize: '29px',
+            color: 'black',
+            align: 'left',
+            padding: {
+                top: 10,
+                bottom: 5,
+            },
+            fixedWidth: 200
+        }
+
+        //initilize score;
+        this.pScore = 0;
+        this.pointText = this.add.text(game.config.width/2 + 200,game.config.height/2 - 270, 'Points: ', textConfig);
+        this.scoreLeft = this.add.text(game.config.width/2 + 350,game.config.height/2 - 270,  this.pScore, textConfig);
+        // make ground tiles group
+        this.ground = this.add.group();
+        for(let i = 0; i < game.config.width; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'sprite', 'block').setScale(SCALE).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        // put another tile sprite above the ground tiles
+        this.groundScroll = this.add.tileSprite(0, game.config.height-tileSize, game.config.width, tileSize, 'ground').setOrigin(0);
+
+        // set up my alien son 👽
+        this.player = this.physics.add.sprite(game.config.width/6, game.config.height/2-tileSize, 'sprite', 'side').setScale(SCALE);
+
+        // set up Phaser-provided cursor key input
+        cursors = this.input.keyboard.createCursorKeys();
+        
+        // add physics collider
+        this.physics.add.collider(this.player, this.ground);
+
+        addScore = this.time.addEvent({ delay: 1000, callback: this.addToScore, callbackScope: this, loop: true }); //calls addToScore every second
+    }
+
+    addToScore() {
+        
+        this.pScore += 10;
+    }
+
+    update() {
+        // update tile sprites (tweak for more "speed")
+        this.background.tilePositionX += this.SCROLL_SPEED;
+        this.groundScroll.tilePositionX += this.SCROLL_SPEED;
+
+		// check if alien is grounded
+	    this.player.isGrounded = this.player.body.touching.down;
+	    // if so, we have jumps to spare
+	    if(this.player.isGrounded) {
+            //this.player.anims.play('walk', true);
+	    	this.jumps = this.MAX_JUMPS;
+	    	this.jumping = false;
+	    }
+        // allow steady velocity change up to a certain key down duration
+	    if(this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(cursors.space, 150)) {
+	        this.player.body.velocity.y = this.JUMP_VELOCITY;
+	        this.jumping = true;
+	    } 
+        // finally, letting go of the UP key subtracts a jump
+	    if(this.jumping && Phaser.Input.Keyboard.UpDuration(cursors.space)) {
+	    	this.jumps--;
+	    	this.jumping = false;
+	    }
+        this.scoreLeft.text = this.pScore;
+    }
+}
