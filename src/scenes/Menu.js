95074065a42678b6b42c8ada63ca55f4e94a8442
jmpollar <103337804+jmpollar@users.noreply.github.com>
2022-04-24 16:07:04 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,7 +3,9 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
     preload() {
-        this.load.audio('baby_song', './assets/Baby_song.mp3');
+        //this.load.audio('baby_song', './assets/Baby_song.mp3');]
+        //load background music
+        this.load.audio('bg_music', './assets/Baby_song 2.1.m4a');
     }
     
     create(){
