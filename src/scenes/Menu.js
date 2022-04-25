eee2faea9dfca74509c74f30d73cbd129ffb8702
jmpollar <103337804+jmpollar@users.noreply.github.com>
2022-04-24 20:54:06 -0700

    Revert "Update Menu.js"
    This reverts commit 95074065a42678b6b42c8ada63ca55f4e94a8442.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,9 +3,7 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
     preload() {
-        //this.load.audio('baby_song', './assets/Baby_song.mp3');]
-        //load background music
-        this.load.audio('bg_music', './assets/Baby_song 2.1.m4a');
+        this.load.audio('baby_song', './assets/Baby_song.mp3');
     }
     
     create(){
